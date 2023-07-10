de621a794d716631b2bc103d62825999272ad9d5
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-07-10 22:10:12 +0100

    started work on controllers

---
+++ controllers/home-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const {  } = require('../models');
+
+
+
+router.get('/', async (req, res) => {
+    try {
+        
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+});
